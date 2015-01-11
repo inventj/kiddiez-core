15b5b341d69ef1c34cdfb2768112f564ecdc02db
Joeri <peeters.joeri@gmail.com>
2015-01-11 22:46:59 +0100

    code

---
+++ src/main/java/be/inventj/kiddiez/config/KiddiezInitializer.java
@@ -0,0 +1,45 @@
+package be.inventj.kiddiez.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class KiddiezInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] {
+        };
+    }
+
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { WebConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+
+        CORSFilter cors = new CORSFilter();
+
+        return new Filter[] { characterEncodingFilter, cors};
+    }
+}
